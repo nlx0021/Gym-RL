270ba8e8cab4aa9d2603350950ff94548242cbe7
nlx0021 <99457474+nlx0021@users.noreply.github.com>
2024-05-29 19:52:41 +0800

    Add files via upload

---
+++ trainer/player.py
@@ -0,0 +1,13 @@
+import numpy as np
+import gymnasium as gym
+
+class Player():
+    
+    def __init__(self):
+        pass
+    
+    def play(self,
+              net,
+              env):
+        
+        pass
---
+++ trainer/trainer.py
@@ -0,0 +1,15 @@
+import numpy as np
+import gymnasium as gym
+
+class Trainer():
+    
+    def __init__(self):
+        pass
+    
+    def train(self,
+              net,
+              env,
+              iters_n):
+        
+        # The main training process.
+        pass
