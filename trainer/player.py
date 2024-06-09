ba898e675a7eee79cbd2134163d17d6aceddbecf
nlx0021 <nlxliwenye@126.com>
2024-06-09 21:04:36 +0800

    player

--- trainer/player.py
+++ trainer/player.py
@@ -14,12 +14,18 @@ class Player():
               env):
         
         observation, info = env.reset()        
+        game_ct = 0
+        total_reward = 0
         while True:
-                # action = env.action_space.sample()  # agent policy that uses the observation and info
-                observation = torch.tensor(observation, dtype=torch.float32)
-                value, policy = net(observation)
-                action = sample_action(policy)
-                observation, reward, terminated, truncated, info = env.step(action)
-                # import pdb; pdb.set_trace()
-                if terminated or truncated:
-                    observation, info = env.reset()        
+            # action = env.action_space.sample()  # agent policy that uses the observation and info
+            observation = torch.tensor(observation, dtype=torch.float32)
+            value, policy = net(observation)
+            action = sample_action(policy)
+            observation, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+            # import pdb; pdb.set_trace()
+            if terminated or truncated:
+                observation, info = env.reset()        
+                print("Over: %d. The reward is %f" % (game_ct, total_reward))
+                game_ct += 1
+                total_reward = 0
